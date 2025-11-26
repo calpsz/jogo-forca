2fb11f48d7a401e28c5761b3612741c472478f13
katsunny <carolopes2002@gmail.com>
2025-11-26 13:51:29 -0300

    convertendo variaveis tentativas, palavras e chutes em globais

--- forca.c
+++ forca.c
@@ -6,21 +6,30 @@
 -> * acessa o conteudo dentro do ponteiro, entra no endereco passado
 */
 
+
+/*variaveis globais*/
+int tentativas = 0;
+char palavrasecreta[20];
+    
+//armazena as letras que foram chutadas
+char chutes[26];
+
+
 void abertura(){
     printf("*****************\n");
     printf("* Jogo de Forca *\n");
     printf("*****************\n\n");
 }
 
-void chuta(char chutes[26], int* tentativas) {
+void chuta() {
     char chute;
     scanf(" %c", &chute);
 
-    chutes[*tentativas] = chute;
-    (*tentativas)++;
+    chutes[tentativas] = chute;
+    tentativas++;
 }
 
-int jachutou(char letra, char chutes[26], int tentativas){
+int jachutou(char letra){
     int achou = 0;
 
     for (int j = 0; j < tentativas; j++){
@@ -33,11 +42,11 @@ int jachutou(char letra, char chutes[26], int tentativas){
     return achou;
 }
 
-void desenhaforca(char palavrasecreta[20], char chutes[26], int tentativas){
+void desenhaforca(){
     for(int i = 0; i < strlen(palavrasecreta); i++){
 
         // a letra ja foi chutada?
-        int achou = jachutou(palavrasecreta[i], chutes, tentativas);
+        int achou = jachutou(palavrasecreta[i]);
         
         if(achou){
             printf("%c ", palavrasecreta[i]);
@@ -49,31 +58,24 @@ void desenhaforca(char palavrasecreta[20], char chutes[26], int tentativas){
     printf("\n");
 }
 
-void escolhepalavra(char palavrasecreta[20]){
+void escolhepalavra(){
     sprintf(palavrasecreta, "MELANCIA");
 }
 
-int main (){
-    char palavrasecreta[20];
-
+int main (){  
     int acertou = 0;
     int enforcou = 0;
 
-    //armazena as letras que foram chutadas
-    char chutes[26];
-
-    int tentativas = 0;
-    
-    escolhepalavra(palavrasecreta);
+    escolhepalavra();
     abertura();
 
     do {
 
         // imprime a palavra secreta
-        desenhaforca(palavrasecreta, chutes, tentativas);
+        desenhaforca();
 
         // captura um chute
-        chuta(chutes, &tentativas);
+        chuta();
 
     } while (!acertou && !enforcou);
 
